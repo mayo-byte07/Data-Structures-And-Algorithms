bb9f94dcd38917eb09e945159619643c58820a4f
Piyush Singh <piyushsinghgujjar2106@gmail.com>
2026-09-19 04:39:56 +0530

    Time: 302 ms (94.57%), Space: 67.2 MB (85.85%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    melted_df = pd.melt(
+        products, 
+        id_vars=['product_id'], 
+        var_name='store', 
+        value_name='price'
+    )
+    result = melted_df.dropna(subset=['price'])
+    
+    return result
